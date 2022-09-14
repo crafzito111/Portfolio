903736a648e836c54c830cf19feea983e6bfad19
crafzito111 <carlosluisprad2@gmail.com>
2022-09-14 19:46:30 -0400

    Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import imageYo from "../assets/yooo.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
+
 
 const Home = () => {
   return (
@@ -21,7 +22,7 @@ const Home = () => {
                 </p>
 
                 <div>
-                    <Link to='portfolio' smooth duration={600} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
+                    <Link to='portfolio' smooth duration={600} className='group cursor-pointer text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
                         Portfolio
                         <span className='group-hover:rotate-90 duration-300'>
                         <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
