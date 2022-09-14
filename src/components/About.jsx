712a43d606368230619d57f8af71ab01002505d3
crafzito111 <carlosluisprad2@gmail.com>
2022-09-14 16:51:51 -0400

    a

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <div name='about' className="About__margin w-full  h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+
+          <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+              <div className='pb-8'>
+                  <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+                    </div>
+
+              <p className="text-xl mt-20">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum temporibus iure corporis quis vitae, reprehenderit qui facere minima error libero eius animi explicabo quae obcaecati itaque distinctio possimus, ad delectus facilis. Autem corrupti dolores tempora vel maiores eos, cumque illo nostrum laborum nisi, esse debitis voluptatem cum doloribus tenetur sit!
+            </p>
+<br />
+
+<p className='text-xl'>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel itaque at tenetur dignissimos aliquam dolor earum velit voluptatem ex ratione libero, voluptates dolorum maiores reiciendis ullam non numquam? Adipisci, autem fugiat eum cupiditate, molestias omnis unde excepturi deserunt nisi quidem amet ipsa quo nam delectus impedit ut culpa tenetur totam.
+</p>
+            </div>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import imageYo from "../assets/yooo.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+      <div name="home" className="min-h-screen flex items-center w-full bg-gradient-to-b from-black via-black to-gray-800">
+
+          <div className="max-w-screen-lg   mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row textVer">
+              <div className=" flex  flex-col justify-center  h-full mt-20">
+                  <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                    I´m a FrontEnd Developer
+                </h2>
+                  <p className="text-gray-500 py-4 ">
+                      Hola!✋ Soy Carlos un Desarrollador Front-end.
+
+                      Me caracterizo por ser una persona resiliente dispuesto a trabajar en equipo de la cual siempre esta dispuesto aprender y escuchar distintos puntos de vista.
+
+                      Y cada dia motivado a aprender cosas nuevas🚀
+                </p>
+
+                <div>
+                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
+                        Portfolio
+                        <span className='group-hover:rotate-90 duration-300'>
+                        <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div className='imageYoo'>
+                  <img src={imageYo} alt="My picture" className="rounded-2xl  mx-auto w-2/3 md:w-full " />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
